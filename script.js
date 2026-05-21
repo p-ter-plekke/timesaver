bd5f30f774b299014461021f2f970f55e37eacdf
p-ter-plekke <254936090+p-ter-plekke@users.noreply.github.com>
2026-05-21 16:19:12 +0200

    Update goal time when setting price/hourly wage

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ const circumference = 2 * Math.PI * Number(circle.getAttribute("r"));
 circle.style.strokeDasharray = circumference;
 circle.style.strokeDashoffset = circumference;
 
+const inputPriceBox = document.querySelector("#inputprice");
+const setPriceButton = document.querySelector(".set-price-button");
+const inputWageBox = document.querySelector("#inputwage");
+const setWageButton = document.querySelector(".set-wage-button");
 const playButton = document.querySelector(".playbutton");
 const pauseButton = document.querySelector(".pausebutton");
-const inputWageBox = document.querySelector("#inputwage");
-const inputPriceBox = document.querySelector("#inputprice");
-const setWageButton = document.querySelector(".set-wage-button");
-const setPriceButton = document.querySelector(".set-price-button");
+let goalTimer = document.querySelector("#goal-timer");
 
 let price = 0;
 let hourWage = 0;
@@ -23,12 +24,18 @@ let isRunning = false;
 setPriceButton.addEventListener("click", () => {
     price = Number(inputPriceBox.value);
     totalSeconds = price / (hourWage / 3600);
+    updateTimers();
 });
 
 setWageButton.addEventListener("click", () => {
     hourWage = Number(inputWageBox.value);
     totalSeconds = price / (hourWage / 3600);
+    updateTimers();
 });
+
+function updateTimers() {
+    goalTimer.textContent = totalSeconds / 3600;
+};
 
 function startTimer() {
     if (isRunning) return;
