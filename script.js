96f6e408ad7a527986a7629c176748762636ba0c
p-ter-plekke <254936090+p-ter-plekke@users.noreply.github.com>
2026-05-21 14:40:03 +0200

    Add function that calculates needed time and fills circle in this amount of time

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const circumference = 2 * Math.PI * Number(circle.getAttribute("r"));
 
 circle.style.strokeDasharray = circumference;
 let progress = circumference;
+const hourWage = 5;
+const price = 1;
+const totalSeconds = price / (hourWage / 3600);
 
 const interval = setInterval(() => {
-    progress -= 1;
+    progress -= (circumference / totalSeconds);
     circle.style.strokeDashoffset = progress;
 
     if (progress <= 0) {
         clearInterval(interval);
     }
-}, 10);
+}, 1000); // update per seconde
+
+
 
