9e9c386085cec5a91b61700dfa0f9d7fe9aa3ac4
p-ter-plekke <254936090+p-ter-plekke@users.noreply.github.com>
2026-06-03 12:39:23 +0200

    Close modal when clicking overlay

--- script.js
+++ script.js
@@ -98,6 +98,13 @@ openModalButtons.forEach(button => {
     });
 });
 
+overlay.addEventListener("click", () => {
+    const modals = document.querySelectorAll(".modal.active");
+    modals.forEach(modal => {
+        closeModal(modal);
+    });
+});
+
 closeModalButtons.forEach(button => {
     button.addEventListener("click", () => {
         const modal = button.closest(".modal");
